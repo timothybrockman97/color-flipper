4e04ea0892e328b4cb89b6a49bae4b4ad401ef8b
timothybrockman97 <95176662+timothybrockman97@users.noreply.github.com>
2022-09-05 12:19:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
-const btb = document.getElementById("btn");
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
 btn.addEventListener("click", function () {
- let hexColor = "#";
-  for (let i = 0; i < 6; i++) {
-    hexColor += hex[getRandomNumber()];
-  }
-  color.textContent = hexColor;
-  document.body.style.backgroundColor = hexColor;
+  const randomNumber = getRandomNumber();
+  // console.log(randomNumber);
+
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
 });
 
 function getRandomNumber() {
-  return Math.floor(Math.random() * hex.length)
-};
+  return Math.floor(Math.random() * colors.length);
+}
